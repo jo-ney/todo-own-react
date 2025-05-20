0b660deae7f5336d5a323d7f0a264c0a08bd9c0f
Joney <joneyabrahams@gmail.com>
2025-05-20 23:11:53 +0530

    commit 10 loading animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Todo from './components/Todo';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay (e.g., wait for assets or data)
+    window.addEventListener("load", () => {
+      // Give a small timeout if you want smoother transition
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 800); // optional delay
+    });
+  }, []);
+
   return (
-    <div className="App">
+    <>
+    {isLoading ? (
+      <div className="loader-wrapper">
+      <div className="spinner"></div>
+    </div>
+    ) : (
+      <div className="app-content">
       <Todo/>
     </div>
+    )}
+  </>
   );
 }
 
